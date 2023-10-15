5c4013d2ca7b3511e058ed1cf02880a680edb503
SchwapTobi <tobias.schwap@gmail.com>
2023-10-15 15:49:12 +0200

    train model

--- fleet/app/models.py
+++ fleet/app/models.py
@@ -5,5 +5,24 @@ class Train(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
 
+    # Store wagon IDs as a comma-separated string
+    wagon_ids = db.Column(db.String, nullable=True)
+
+    def __init__(self, name, wagon_ids=None):
+        self.name = name
+        self.wagon_ids = wagon_ids
+
+    def get_wagon_ids(self):
+        if self.wagon_ids:
+            return [int(id) for id in self.wagon_ids.split(",")]
+        else:
+            return []
+
+    def add_wagon_id(self, wagon_id):
+        if not self.wagon_ids:
+            self.wagon_ids = str(wagon_id)
+        else:
+            self.wagon_ids += f",{wagon_id}"
+
     def __repr__(self):
         return f"Train(id={self.id}, name={self.name})"
