ee8202d41c05e9b77686a1b25e084d211313e395
SchwapTobi <tobias.schwap@gmail.com>
2023-11-19 09:39:44 +0100

    config fix

--- fleet/config.py
+++ fleet/config.py
@@ -3,7 +3,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
-# used for database handling of the fleet database
-class Config:
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'fleet.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
