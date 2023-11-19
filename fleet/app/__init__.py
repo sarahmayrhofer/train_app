eaffd2aa79cc65dfe0c800df0417b38565041240
SchwapTobi <tobias.schwap@gmail.com>
2023-11-19 09:40:41 +0100

    fix project structure

---
+++ fleet/app/__init__.py
@@ -0,0 +1,21 @@
+import os
+
+from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+from fleet.config import Config
+
+# Create a Flask application instance
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# fixes: RuntimeError: A secret key is required to use CSRF
+# https://stackoverflow.com/questions/47687307/how-do-you-solve-the-error-keyerror-a-secret-key-is-required-to-use-csrf-whe
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
+
+# Import routes after initializing the Flask app and SQLAlchemy
+from fleet.app import routes
